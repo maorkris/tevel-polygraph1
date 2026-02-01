5df4dd9011f81146df3e8369cb7693b306022013
maor_kris <142143890+maorkris@users.noreply.github.com>
2026-02-01 23:02:27 +0200

    Delete js/accessibility-loader.js

--- js/accessibility-loader.js
+++
@@ -1,7 +0,0 @@
-// טעינת תוסף נגישות Nagich לכל הדפים
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://nagich.co.il/plugin/nagich.js';
-    script.async = true;
-    document.head.appendChild(script);
-})();
