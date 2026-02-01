4134e2bddc028302efa9a4c7a40d85a94639b2e6
maor_kris <142143890+maorkris@users.noreply.github.com>
2026-02-01 22:48:25 +0200

    Create accessibility-loader.js

---
+++ js/accessibility-loader.js
@@ -0,0 +1,7 @@
+// טעינת תוסף נגישות Nagich לכל הדפים
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://nagich.co.il/plugin/nagich.js';
+    script.async = true;
+    document.head.appendChild(script);
+})();
